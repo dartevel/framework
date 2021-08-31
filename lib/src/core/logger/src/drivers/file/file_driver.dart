7d59c53890c52b8a4b119ed05f219762a1868f56
miladimos <milad.jafari6210@gmail.com>
2021-08-30 17:07:20 -0700

    add logger contract

--- lib/src/core/logger/src/drivers/file/file_driver.dart
+++ lib/src/core/logger/src/drivers/file/file_driver.dart
@@ -1,5 +1,48 @@
 import '../log_driver.dart';
 
-class FileDriver extends LogDriver{
-  
+class FileDriver extends LogDriver {
+  @override
+  void alert($message) {
+    // TODO: implement alert
+  }
+
+  @override
+  void critical($message) {
+    // TODO: implement critical
+  }
+
+  @override
+  void debug($message) {
+    // TODO: implement debug
+  }
+
+  @override
+  void emergency($message) {
+    // TODO: implement emergency
+  }
+
+  @override
+  void error($message) {
+    // TODO: implement error
+  }
+
+  @override
+  void info($message) {
+    // TODO: implement info
+  }
+
+  @override
+  void log($level, $message) {
+    // TODO: implement log
+  }
+
+  @override
+  void notice($message) {
+    // TODO: implement notice
+  }
+
+  @override
+  void warning($message) {
+    // TODO: implement warning
+  }
 }
--- lib/src/core/logger/src/drivers/log_driver.dart
+++ lib/src/core/logger/src/drivers/log_driver.dart
@@ -1,3 +1,4 @@
-abstract class LogDriver {
+import 'package:dartevel_framework/src/core/contracts/logger/logger_contract.dart';
+abstract class LogDriver implements LoggerContract {
   
 }
