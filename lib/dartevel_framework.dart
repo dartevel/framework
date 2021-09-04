4342f5a6e0658acf8f28c2690e0153af8c4e1881
miladimos <milad.jafari6210@gmail.com>
2021-09-03 21:30:14 -0700

    add lib struct

--- lib/dartevel_framework.dart
+++ lib/dartevel_framework.dart
@@ -1,7 +1,8 @@
 library dartevel.framework;
 
+
 export 'src/core/contracts/contracts.dart';
 export 'src/core/cache/cache.dart';
 export 'src/core/config/config.dart';
 export 'src/core/logger/logger.dart';
-export 'src/core/validator/validator.dart';
+export 'src/core/validator/lib/validator.dart';
