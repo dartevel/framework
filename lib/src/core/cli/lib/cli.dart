13d8eddff96fdaaa5d4ecb58cda14fb361a94349
miladimos <milad.jafari6210@gmail.com>
2021-09-06 22:01:01 -0700

    cli add figlet banners

--- lib/src/core/cli/lib/cli.dart
+++ lib/src/core/cli/lib/cli.dart
@@ -1,4 +1,4 @@
 library dartevel.framework.cli;
 
 
-export 'package:dartevel_cli/commands/commands.dart';
+export 'src/commands/commands.dart';
--- lib/src/core/cli/lib/src/commands/new.dart
+++ lib/src/core/cli/lib/src/commands/new.dart
@@ -1,4 +1,20 @@
-class NewCommand 
-{
-  
+import 'package:args/command_runner.dart';
+
+class NewCommand extends Command {
+  final _description = 'create new project';
+  final _name = 'new';
+
+  @override
+  String get description => _description;
+
+  @override
+  String get name => _name;
+
+  CommitCommand() {
+    argParser.addFlag('api', abbr: 'a');
+  }
+
+  void run() {
+    print(argResults);
+  }
 }
